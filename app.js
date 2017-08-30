26e51252ee11f631aba7d8169e61a1a112730be7
daviddinnison <david.g.dinnison@gmail.com>
2017-08-30 15:38:41 -0400

    app.js pushing

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const Array = require('./Array');
 
 const arr = new Array();
 arr.push(1);
+console.log('-----first push', arr);
+console.log('pointer', arr)
+arr.push(2);
+arr.push(3);
+arr.push(4);
+console.log('----second push', arr);
+arr.push(5);
+console.log('pointer', arr)
+arr.push(6);
+console.log('pointer', arr)
+arr.push(7);
+console.log('pointer', arr)
+arr.push(8);
+console.log('pointer', arr)
+arr.push(9);
+console.log('pointer', arr)
+arr.push(10);
+console.log('pointer', arr)
+arr.push(11);
+console.log('pointer', arr)
+arr.push(12);
+console.log('pointer', arr)
+arr.push(13);
+console.log('pointer', arr)
 
-
-console.log(arr);
+console.log('----third push', arr);
